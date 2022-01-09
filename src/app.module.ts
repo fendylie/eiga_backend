52dad9ff008dbf1bb1bc17fcc8fff2f11425a1bf
effendy28 <effendyrzr@gmail.com>
2022-01-09 17:07:12 +0700

    update db setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,11 @@ import { Movie } from './movies/entities/movie.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: +process.env.DATABASE_PORT || 3306,
-      username: process.env.DATABASE_USERNAME || 'root',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME || 'eiga',
+      database: process.env.DATABASE_NAME,
       namingStrategy: new SnakeNamingStrategy(),
       entities: [User, Movie],
       synchronize: !['production'].includes(process.env.NODE_ENV),
